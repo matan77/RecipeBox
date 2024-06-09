88abe34283c17c1c70691887cae802e2cfd7eaad
matan77 <matan54828482@gmail.com>
2024-06-09 20:29:30 +0300

    feat: added navigation component

--- app/src/main/java/com/example/recipebox/MainActivity.java
+++ app/src/main/java/com/example/recipebox/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.recipebox;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.recipebox.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -19,11 +23,25 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        navController.navigate(R.id.navigation_login);
+
+
+
+
+
+
+
+
+
+
     }
 }
